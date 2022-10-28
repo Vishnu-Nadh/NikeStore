6955986fe785d04f511e0b3592584c5f3058cdc6
Vishnu-Nadh <vishnunadh222@gmail.com>
2022-10-28 15:22:48 +0530

    completed sales component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { Hero, Sales } from "./components";
-import { heroapi } from "./data/data";
+import {
+  heroapi,
+  popularsales,
+  toprateslaes,
+  highlight,
+  sneaker,
+  story,
+  footerAPI,
+} from "./data/data.js";
 
 const App = () => {
   return (
     <>
-      <main>
+      <main className="flex flex-col gap-24 relative">
         <Hero heroapi={heroapi} />
-        <Sales />
+        <Sales endpoint={popularsales} ifExists />
+        <Sales endpoint={toprateslaes} />
       </main>
     </>
   );
